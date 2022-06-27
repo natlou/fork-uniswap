70f1e6b42bc0b17f9316d54c1bd6c7aab943f06c
natlou <natloudev@gmail.com>
2022-06-27 04:17:57 -0700

    added some necessary tests

--- test/UniswapV2Router02.spec.ts
+++ test/UniswapV2Router02.spec.ts
@@ -8,7 +8,7 @@ import IUniswapV2Pair from '@uniswap/v2-core/build/IUniswapV2Pair.json'
 import { v2Fixture } from './shared/fixtures'
 import { expandTo18Decimals, getApprovalDigest, MINIMUM_LIQUIDITY } from './shared/utilities'
 
-import DeflatingERC20 from '../build/DeflatingERC20.json'
+import DeflatingERC20 from '../v2-periphery/build/DeflatingERC20.json'
 import { ecsign } from 'ethereumjs-util'
 
 chai.use(solidity)
